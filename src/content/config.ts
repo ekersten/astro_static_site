7c39517e5fd52ac4fbc22f9cadb653a6bb2dda79
Eric Kersten <ekersten@gmail.com>
2025-08-22 16:11:15 -0300

    characters

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,48 @@
 import { defineCollection, z } from 'astro:content';
+import slug from 'slug'
 
 const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    cover: z.string().optional(),
-    excerpt: z.string(),
-    published: z.boolean().default(true),
-    tags: z.array(z.string()).default([]),
-  }),
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        cover: z.string().optional(),
+        excerpt: z.string(),
+        published: z.boolean().default(true),
+        tags: z.array(z.string()).default([]),
+    }),
 });
 
+const characters = defineCollection({
+    loader: async () => {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        if (!response.ok) {
+            throw new Error('Failed to fetch characters');
+        }
+        const data = await response.json();
+        return data.results.map((character: any) => ({
+            id: slug(character.name, { lower: true }),
+            name: character.name,
+            image: character.image,
+            species: character.species,
+            status: character.status,
+            origin: character.origin.name,
+            location: character.location.name,
+
+        })
+        );
+    },
+    schema: ({ image }) => z.object({
+        id: z.string(),
+        name: z.string(),
+        image: image(),
+        species: z.string(),
+        status: z.string(),
+        origin: z.string(),
+        location: z.string(),
+    })
+})
+
 export const collections = {
-  blog,
+    blog, characters
 };
